743c6742efa713ee20a473a1dfced8794509c3e6
Ng Thanh Cong <nxgthanhcong@gmail.com>
2021-08-12 19:56:53 +0700

    first commit

---
+++ GetImg/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GetImg
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var workbook = new XLWorkbook(@"C:\Users\nxgth\OneDrive\Desktop\ReadIMG\Submit Inlinks.xlsx");
+            var worksheet = workbook.Worksheets.Worksheet("Sheet1");
+            int row = 2;
+            while (worksheet.Cell(row, 1).Value.ToString() != "")
+            {
+                WebClient client = new WebClient();
+                client.DownloadFile(new Uri(worksheet.Cell(row, 1).Value.ToString()), @"C:\Users\nxgth\OneDrive\Desktop\ReadIMG\FolderImgs\" + worksheet.Cell(row, 2).Value.ToString() + ".jpg");
+                row++;
+            }
+        }
+    }
+}
